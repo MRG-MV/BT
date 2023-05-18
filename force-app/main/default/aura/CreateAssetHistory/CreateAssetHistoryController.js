c82247bd2c5eeede456f108dee1114ccd3176853
Mitrajsinh Gohil <mitraj.g@mvclouds.com>
2023-05-18 17:19:50 +0530

    18 May 2023 Evening before changes

---
+++ force-app/main/default/aura/CreateAssetHistory/CreateAssetHistoryController.js
@@ -0,0 +1,107 @@
+({
+    doInit : function(component, event, helper) {
+        component.set("v.Spinner", true);
+        console.log('recordId: ' + component.get("v.recordId"));
+        var action = component.get("c.getPreviousAssetHistory");
+        action.setParams({
+            assetId: component.get("v.recordId")
+        });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            console.log({ state });
+            if(state=='SUCCESS'){    
+                var result = response.getReturnValue();
+                console.log({ result });
+                if(result == null){
+                    component.set("v.showNewModal", true);
+                    component.set("v.Spinner", false);
+                }else{
+                    console.log('result: ' + JSON.stringify(result));
+                    component.set("v.PreviousAssetHistory", result);
+                    component.set("v.showOldModal", true);
+                    component.set("v.Spinner", false);
+                }
+            }else{
+                helper.showToast("Error", "Error", "Something went wrong", "5000");
+                $A.get("e.force:closeQuickAction").fire();
+                component.set("v.Spinner", false);
+            }
+        }
+        );
+        $A.enqueueAction(action);
+
+    },
+
+    createRecord: function(component, event, helper) {
+        try {
+            component.set("v.Spinner", true);
+            var AssetHistoryFields = component.get('v.AssetHistory');
+            AssetHistoryFields.buildertek__Asset_Manager__c = component.get("v.recordId");
+            console.log('CreateAssetHistoryController.createRecord: AssetHistoryFields: ' + JSON.stringify(AssetHistoryFields));
+            debugger;
+            var action = component.get("c.CreateAssetHistoryRecord");
+            action.setParams({
+                AssetHistory: AssetHistoryFields
+            });
+            action.setCallback(this, function(response) {
+                var state = response.getState();
+                console.log({ state });
+                var result = response.getReturnValue();
+                if(state=='SUCCESS'){
+                    $A.get("e.force:closeQuickAction").fire();
+                    $A.get('e.force:refreshView').fire();
+                    helper.showToast("Success", "Success", "Your Asset History is created", "5000");
+                    var urlEvent = $A.get("e.force:navigateToURL");
+                    urlEvent.setParams({
+                        "url": "/lightning/r/Asset_History__c/" + result + "/view?ws=%2Flightning%2Fr%2Fbuildertek__Asset_Manager__c%2F" + component.get("v.recordId") + "%2Fview" 
+                    });
+                    urlEvent.fire();
+                }else{
+                    helper.showToast("Error", "Error", "Record not inserted", "5000");
+                    $A.get("e.force:closeQuickAction").fire();
+                }
+                component.set("v.Spinner", false);
+                
+            });
+            $A.enqueueAction(action);
+        } catch (e) {
+            console.log({ e });
+        }
+    },
+
+    updateRecord: function(component, event, helper) {
+        try {
+            component.set("v.Spinner", true);
+            var AssetHistoryFields = component.get('v.PreviousAssetHistory');
+            console.log('CreateAssetHistoryController.updateRecord: AssetHistoryFields: ' + JSON.stringify(AssetHistoryFields));
+            debugger;
+            var action = component.get("c.UpdateAssetHistoryRecord");
+            action.setParams({
+                PreviousAssetHistory: AssetHistoryFields
+            });
+            action.setCallback(this, function(response) {
+                var state = response.getState();
+                console.log({ state });
+                var result = response.getReturnValue();
+                console.log('result: ' + JSON.stringify(result));
+                if(state=='SUCCESS' && !result.startsWith('Error') ){
+                    helper.showToast("Success", "Success", "Your Asset History is updated", "5000");
+                    component.set("v.showOldModal", false);
+                    component.set("v.showNewModal", true);
+                }else{
+                    helper.showToast("Error", "Error", "Record not updated", "5000");
+                    $A.get("e.force:closeQuickAction").fire();
+                }
+                component.set("v.Spinner", false);
+                
+            });
+            $A.enqueueAction(action);
+        } catch (e) {
+            console.log({ e });
+        }
+    },
+
+    closePopup: function(component, event, helper) {
+        $A.get("e.force:closeQuickAction").fire();
+    },
+})
