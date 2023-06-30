70d134351d330691e9b88c7c0ecdfd6c89f1a3c4
Mitrajsinh Gohil <mitraj.g@mvclouds.com>
2023-06-30 12:21:00 +0530

    30 june morning

--- force-app/main/default/lwc/bT_HomePage/bT_HomePage.js
+++ force-app/main/default/lwc/bT_HomePage/bT_HomePage.js
@@ -1,14 +1,31 @@
 import { LightningElement, track } from 'lwc';
 import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
 import PDF_Resource from '@salesforce/resourceUrl/Releasenote';
+import designcss from '@salesforce/resourceUrl/designcss'
 
 export default class Qf_guide2 extends LightningElement {
   @track spinnerdatatable = false;
   error_toast = true;
   pdfUrl;
+  activeSections = ['A'];
+  activeSectionsMessage = '';
+  supportname;
+    
+
+    handleSectionToggle(event) {
+        const openSections = event.detail.openSections;
+
+        if (openSections.length === 0) {
+            this.activeSectionsMessage = 'All sections are closed';
+        } else {
+            this.activeSectionsMessage =
+                'Open sections: ' + openSections.join(', ');
+        }
+    }
 
     connectedCallback() {
         this.pdfUrl = PDF_Resource;
+        loadStyle(this, designcss);
     }
 
   renderedCallback() {
@@ -43,6 +60,89 @@ export default class Qf_guide2 extends LightningElement {
     this.template.querySelector('[data-tab-id="' + target + '"]').classList.add("active-tab");
     this.template.querySelector('[data-id="' + target + '"]').classList.add("active-tab-content");
   }
+
+  Support_name(event) {
+    this.supportname = event.target.value;
+    this.name_msg = true;
+}
+name_msg = true;
+email;
+subject;
+message;
+email_msg = true;
+Message_msg = true;
+subject_msg = true;
+Message_msg = true;
+Support_email(event) {
+  this.email = event.target.value;
+  this.email_msg = true;
+}
+Quickbot_cc(event) {
+  this.quickbotcc = event.target.value;
+  this.cc_msg = true;
+}
+Support_message(event) {
+  this.message = event.target.value;
+  this.Message_msg = true;
+
+}
+Support_subject(event) {
+  this.subject = event.target.value;
+  this.subject_msg = true;
+
+}
+onSubmit() {
+
+  var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  var validation1 = pattern.test(this.email);
+  var validation2 = true;
+  this.supportname = this.supportname.trim();
+  this.subject = this.subject.trim();
+  this.message = this.message.trim();
+  if ((this.supportname == undefined) || (this.supportname == '')) {
+      this.name_msg = false;
+  } else if (validation1 == false) {
+      this.email_msg = false;
+  } else if ((validation2 == false)) {
+      this.cc_msg = false;
+  } else if (this.subject == undefined || (this.subject == '')) {
+      this.subject_msg = false;
+  } else if (this.message == undefined || (this.message == '')) {
+      this.Message_msg = false;
+  } else {
+      this.email_msg = true;
+      // sendemail({
+      //         name: this.supportname,
+      //         email: this.email,
+      //         subject: this.subject,
+      //         body: this.message,
+      //         fname: this.FName,
+      //         fbase64: this.FBase64,
+      //     })
+      //     .then(result => {
+      //         if(result == 'success'){
+      //             this.emailsend = true;
+      //             this.dispatchEvent(new CustomEvent('botclose', {
+      //                 detail: this.emailsend
+      //             }));
+      //             this.dispatchEvent(new CustomEvent('success'));
+      //         }else{
+      //             this.error_toast = true;
+      //             let toast_error_msg = 'Email was not sent, Something went wrong.';
+      //             this.template.querySelector('c-toast-component').showToast('error', toast_error_msg, 3000);
+      //         }
+      //     })
+
+      const value = false;
+      const valueChangeEvent = new CustomEvent("valuechange", {
+          detail: {
+              value
+          }
+      });
+      this.dispatchEvent(valueChangeEvent);
+  }
+
+}
 
   
 }
--- force-app/main/default/lwc/bryntum_Gantt/bryntum_Gantt.js
+++ force-app/main/default/lwc/bryntum_Gantt/bryntum_Gantt.js
@@ -2050,6 +2050,7 @@ export default class Gantt_component extends NavigationMixin(LightningElement) {
                 // console.log('Record of endDate =>',{record});
                 // console.log('duration>>>', record.record._data.duration);
                 // console.log('type>>>', record.record._data.type);
+                // console.log('name>>>', record.record._data.name);
                 if (
                   record.record._data.duration >= 1 &&
                   record.record._data.type == "Task" &&
@@ -2058,48 +2059,8 @@ export default class Gantt_component extends NavigationMixin(LightningElement) {
                     // console.log('In if conditon for enddate');
                     var start;
                     var endDate = new Date(record.value);
-                    
-                    var start;
-                  if(typeof(record.record.originalData.startDate) == 'string'){
-                    var syear = record.record.originalData.startDate.split('-')[0];
-                    var smonth = record.record.originalData.startDate.split('-')[1];
-                    var sday = record.record.originalData.startDate.split('-')[2];
-                    // var start2 = new Date(record.record.originalData.startDate.getTime());
-                    console.log('record.record.originalData ==> ',record.record.originalData);
-                    console.log('record.record.originalData.startDate ==> ',record.record.originalData.startDate);
-                    console.log('Type of record.record.originalData.startDate ==> ',typeof(record.record.originalData.startDate));
-
-                    var start = new Date(parseInt(syear), parseInt(smonth) - 1, parseInt(sday) + parseInt(record.record.originalData.duration) - 1);
-                  } else{
-
-                    var start = new Date(record.record.originalData.startDate.getTime());
-
-                  } 
-                  // console.log('record>>>', record.record);
-                  // console.log('name>>>', record.record._data.name);
-                  // console.log('ennDate ',endDate);
-
-                  // console.log('record.record.originalData.startDate.getTime() ==>',record.record.originalData.startDate.getTime());
-                  // var start = new Date(record.record.originalData.startDate.getTime());
-
-                  // let inputDate = record.record.originalData.startDate;s
-                  // console.log('inputDate ',inputDate);
-                  // let parts = inputDate.split("-"); // Split the input date into year, month, and day parts
-
-                  // let year = parts[0];
-                  // let month = parseInt(parts[1]) - 1; // Months in JavaScript are zero-based, so subtract 1
-                  // let day = parts[2];
-
-                  // var formate = new Date(year, month, day).toString();
-                  // console.log('formate ',formate);
-
-
-                  console.log('start >>>',typeof(start));
-                  // console.log('start2 >>>',typeof(start2));
-                  console.log('startDate ',record.record.startDate);
-                  console.log('startDate ',record.record.originalData.startDate);
+                  var start = new Date(record.record.startDate.getTime());
                   var duration = record.record.duration;
-                  console.log('duration ',duration);
                   var eDate = new Date(start);
                   var eDatebefore = endDate;
                   // newly added
@@ -2120,7 +2081,7 @@ export default class Gantt_component extends NavigationMixin(LightningElement) {
                     if (new Date(eDate2).getDay() == 6) {
                       eDate2 = new Date(eDate2.setDate(eDate2.getDate() + 2));
                     }
-                    console.log('custom1',eDate2)
+                    //console.log('custom',eDate2)
                     eDate2 = new Date(eDate2);
                   }
                   endDate.setDate(endDate.getDate() - 1);
@@ -2134,7 +2095,6 @@ export default class Gantt_component extends NavigationMixin(LightningElement) {
                     record.value.setFullYear(eDate2.getFullYear());
                     record.value.setDate(eDate2.getDate() + 1);
                   }
-                  console.log('custom2',eDate2)
                   var eDateafter = endDate;
                   return (
                     months[endDate.getMonth()] +
@@ -2653,11 +2613,11 @@ export default class Gantt_component extends NavigationMixin(LightningElement) {
             ) {
               if (editorContext.column.field == "endDate") {
                 var StartString =
-                  editorContext.record.originalData.startDate.getFullYear() +
+                  editorContext.record._data.startDate.getFullYear() +
                   "-" +
-                  Number(editorContext.record.originalData.startDate.getMonth() + 1) +
+                  Number(editorContext.record._data.startDate.getMonth() + 1) +
                   "-" +
-                  editorContext.record.originalData.startDate.getDate();
+                  editorContext.record._data.startDate.getDate();
                 var endString =
                   editorContext.value.getFullYear() +
                   "-" +
@@ -2699,7 +2659,7 @@ export default class Gantt_component extends NavigationMixin(LightningElement) {
                   editorContext.value.setMonth(dt.getMonth());
                   editorContext.value.setFullYear(dt.getFullYear());
                   var duration = 0;
-                  const date1 = new Date(editorContext.record.originalData.startDate);
+                  const date1 = new Date(editorContext.record._data.startDate);
                   const date2 = new Date(dt);
                   const diffTime = Math.abs(date2 - date1);
                   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
@@ -3321,26 +3281,6 @@ export default class Gantt_component extends NavigationMixin(LightningElement) {
     }
   }
 
-  onDrop({ context }) {
-    const
-        { gantt, grid }                                        = this,
-        { valid, highlightRow, parent, insertBefore, grabbed } = context;
-
-    // If drop was done in a valid location, add the task to Gantt's task store
-    if (valid) {
-        const unplannedTask = grid.getRecordFromElement(grabbed);
-
-        // Remove unplanned task from its current store
-        grid.store.remove(unplannedTask);
-
-        // Insert it to Gantt's task store
-        parent.insertChild(unplannedTask, insertBefore);
-    }
-
-    gantt.disableScrollingCloseToEdges(gantt.timeAxisSubGrid);
-
-    highlightRow?.removeCls('drag-from-grid-target-task-before');
-    gantt.features.taskTooltip.disabled = false;
-}
+ 
 
 }
